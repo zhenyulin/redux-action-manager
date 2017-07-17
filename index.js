746923410e89af443228740c5b857116120bb480
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-07-17 17:10:29 +0100

    update file structure

---
+++ index.js
@@ -0,0 +1,20 @@
+import {
+	defaultAction,
+	asyncActionNames,
+	defaultAsyncActions,
+	actionBundle,
+	asyncActionBundle
+} from 'src/creator';
+
+import {
+	immutableAsyncReducers
+} from 'src/reducer';
+
+export default {
+	defaultAction,
+	asyncActionNames,
+	defaultAsyncActions,
+	actionBundle,
+	asyncActionBundle,
+	immutableAsyncReducers
+};
